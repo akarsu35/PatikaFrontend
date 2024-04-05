e090c1da9bdc40ca8ce1c262409e8f2a6305fed7
CUMHUR AKARSU <cumhurakarsu@gmail.com>
2024-04-05 22:51:03 +0300

    update clock.js

--- week4/saat/clock.js
+++ week4/saat/clock.js
@@ -2,7 +2,37 @@ let name = prompt("adınızı giriniz")
 
 document.querySelector("#myName").innerHTML=`${name.toUpperCase()}`
 
-var time = new Date();
-var days = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
-var dayName = days[time.getDay()]; // Gün ismini al
-document.querySelector("#myClock").innerHTML=time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() + " " +dayName
+// var time = new Date();
+// var days = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
+// var dayName = days[time.getDay()]; // Gün ismini al
+// document.querySelector("#myClock").innerHTML=time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() + " " +dayName
+
+
+function showTime() {
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+    let day = now.getDay();
+    
+    // Saati ayarla
+    hour = hour < 10 ? "0" + hour : hour;
+    // Dakikayı ayarla
+    minute = minute < 10 ? "0" + minute : minute;
+    // Saniyeyi ayarla
+    second = second < 10 ? "0" + second : second;
+    
+    // Günleri diziye dönüştür
+    let days = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
+    // Gün adını al
+    let dayName = days[day];
+    
+    let time = hour + ":" + minute + ":" + second + " " + dayName;
+    
+    document.getElementById("myClock").innerText = time;
+    
+    setTimeout(showTime, 1000); // Her saniye güncelle
+  }
+  
+  // Sayfa yüklendiğinde saat gösterimi
+  window.onload = showTime;
